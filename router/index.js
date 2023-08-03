99af485043c36e0b81b69802ff5bb01814ce96fd
Jason Andres López <117871599+J4ckson95@users.noreply.github.com>
2023-08-02 20:33:46 -0500

    crud users (#5)
    Co-authored-by: Jason López <j4ckson95@hotmail.com>

--- router/index.js
+++ router/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
+import userRouter from "./users.js"
 import moviesRouter from "./movies.routes.js"
 
 const indexRouter = Router()
 
 //configurar las rutas de cada recurso
+indexRouter.use("/users", userRouter)
 
 indexRouter.use("/movies", moviesRouter)
 
