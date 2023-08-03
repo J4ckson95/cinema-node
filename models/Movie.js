132d904394d808364984964d14aa3a68fc4ce988
Mauricio <118229759+Vakipandi@users.noreply.github.com>
2023-08-02 20:13:24 -0500

    crudmovie (#4)

--- models/Movie.js
+++ models/Movie.js
@@ -4,7 +4,7 @@ let collection = 'movie'
 
 let schema = new Schema({
     title: { type: String, required: true },
-    capacity: { type: Types.ObjectId, default: 10000 },
+    capacity: { type:Number, default: 10000 },
     price: { type: Number, default: 1000 }
 }, {
     timestamp: true
--- router/index.js
+++ router/index.js
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import moviesRouter from "./movies.routes.js"
 
 const indexRouter = Router()
 
 //configurar las rutas de cada recurso
+
+indexRouter.use("/movies", moviesRouter)
 
 export default indexRouter
