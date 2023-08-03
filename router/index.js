88ad46368032b5ace5ab17d04966078523f791f3
Jason López <j4ckson95@hotmail.com>
2023-08-02 20:29:11 -0500

    crud users

--- router/index.js
+++ router/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
+import userRouter from "./users.js"
 
 const indexRouter = Router()
 
 //configurar las rutas de cada recurso
+indexRouter.use("/users", userRouter)
 
 export default indexRouter
